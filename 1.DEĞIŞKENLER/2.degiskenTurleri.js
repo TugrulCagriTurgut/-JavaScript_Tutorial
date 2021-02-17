544df4179a2d1be8a07fd5be999588c2c58cb588
Tuğrul Çağrı TURGUT <77591154+TugrulCagriTurgut@users.noreply.github.com>
2021-02-17 18:41:07 +0200

    update

---
+++ 1.DEĞIŞKENLER/2.degiskenTurleri.js
@@ -0,0 +1,47 @@
+                        // DEGISKEN TIPLERI
+//1.Pirimitive Types
+        //String
+        let lastname='Turgut';
+        //console.log(typeof lastname);
+                     
+        //Number
+        let age = 25;
+        let money= 100.53;
+        //console.log(typeof age);
+        //console.log(typeof money);
+
+        //Boolean
+        let isActive = true;
+        //console.log(typeof isActive);
+
+        //null, pirimitive bir deger olmaisna ragmen object olarak gostericek konsalda.Hata imis.
+        let job=null;
+        //console.log(typeof job);
+
+        //undefined
+        let car;
+        //console.log(typeof car);
+
+
+
+ //2.Reference Types-Objects
+        //Array
+        let names=['ali','ahmet','mehmet'];
+        console.log(typeof names);
+
+        //Object
+        let adress={
+            city:'Kocaeli',
+            country:'Turkey'
+        }
+        console.log(typeof adress);
+
+        //Function
+        var calculateAge = function(){
+            return 0;
+        }
+        console.log(typeof calculateAge);
+        
+
+
+
